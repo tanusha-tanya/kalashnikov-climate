3bd5ea603c41d3b4f84e1e431c1cddb3165dc71b
Verevkina T <verevkina@picom.ru>
2018-06-26 20:27:37 +0400

    main

--- app/templates/modules/carousel/carousel.js
+++ app/templates/modules/carousel/carousel.js
@@ -1,3 +1,18 @@
 $( document ).ready(function() {
- $(".carousel__items").owlCarousel();  
+ $(".carousel__items").owlCarousel({
+  center: true,
+  autoWidth:true,
+  margin: 20,
+  responsive:{
+    0:{
+       items:2
+    },
+    768:{
+     items:2
+    },
+    1120:{
+     items:2
+    }
+   }
+ });
 });
