71501f04b2f78a9d5ce9881ea2ec7ef385e07909
Verevkina T <verevkina@picom.ru>
2018-07-06 21:07:00 +0400

    detail

---
+++ app/templates/modules/tabs/tabs.js
@@ -0,0 +1,32 @@
+const tabBtn = document.querySelectorAll(".tab__button");
+const tabInfo = document.querySelectorAll(".tab__info");
+if(tabBtn){
+  for(let i = 0; i < tabBtn.length; i++){
+    tabBtn[i].addEventListener("click", function(){
+      clearTab(tabBtn, "button");
+      clearTab(tabInfo, "info");
+      addTab(tabBtn[i], "button");
+      addTab(tabInfo[i], "info");
+    })
+  };
+}
+
+function clearTab(actived, type){
+  for(let i = 0; i < actived.length; i++){
+    if (actived[i].classList.contains("tab__"+type+"-active")){
+      actived[i].classList.remove("tab__"+type+"-active")
+    }
+  }
+}
+function addTab(el, type){
+  el.classList.add("tab__"+type+"-active");
+}
+(function($){
+  $(window).on("load",function(){
+    $(".tab__table").mCustomScrollbar({
+      axis:"x",
+      theme: "my-theme",
+      scrollButtons:{ enable: true }
+    });
+  });
+})(jQuery);
