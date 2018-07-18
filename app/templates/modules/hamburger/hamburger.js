191c14328e54d76b2b0fa5a5f83418650aad8ea8
Verevkina T <verevkina@picom.ru>
2018-07-18 18:08:31 +0400

    leftmenu

--- app/templates/modules/hamburger/hamburger.js
+++ app/templates/modules/hamburger/hamburger.js
@@ -1,8 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
-const footerMobilemenu = document.querySelector(".footer__mobilemenu");
-const section = document.querySelector(".main__section");
 
 if(hamburger){
+  let footerMobilemenu = document.querySelector(".footer__mobilemenu");
+  let section = document.querySelector(".main__section");
+  let footer = document.querySelector(".footer__top");
   footerMobilemenu.style.display = "none";
   hamburger.onmousedown = function(){
     changeHamburger();
@@ -12,31 +13,31 @@ if(hamburger){
     changeHamburger();
     return
   };
-}
-
-function changeHamburger(){
-  (hamburger.classList.contains("openhamburger"))?
-  closeHamburger():
-  openHamburger()
-}
-function openHamburger(){
-  footerMobilemenu.style.display = "block";
-  setTimeout(function(){
-    hamburger.classList.add("openhamburger");
-    footerMobilemenu.classList.add("openmenu");
-  }, 100);
-  setTimeout(function(){
-    section.classList.add("noscroll");
-  }, 300);
-}
-function closeHamburger(){
-  section.classList.remove("noscroll");
-  setTimeout(function(){
-    hamburger.classList.remove("openhamburger");
-    footerMobilemenu.classList.remove("openmenu");
-  }, 100);
-  setTimeout(function(){
-    footerMobilemenu.style.display = "none";
-  }, 300);
-  closeList();
+  function changeHamburger(){
+    (hamburger.classList.contains("openhamburger"))?
+    closeHamburger():
+    openHamburger()
+  }
+  function openHamburger(){
+    footerMobilemenu.style.display = "block";
+    setTimeout(function(){
+      hamburger.classList.add("openhamburger");
+      footerMobilemenu.classList.add("openmenu");
+    }, 100);
+    setTimeout(function(){
+      section.classList.add("noscroll");
+      footer.classList.add("noscroll");
+    }, 300);
+  }
+  function closeHamburger(){
+    section.classList.remove("noscroll");
+    setTimeout(function(){
+      hamburger.classList.remove("openhamburger");
+      footerMobilemenu.classList.remove("openmenu");
+    }, 100);
+    setTimeout(function(){
+      footerMobilemenu.style.display = "none";
+    }, 300);
+    closeList();
+  }
 }
--- app/templates/modules/leftmenu/leftmenu.js
+++ app/templates/modules/leftmenu/leftmenu.js
@@ -3,7 +3,15 @@ if (leftmenuButton){
   for(let i = 0; i < leftmenuButton.length; i++){
     leftmenuButton[i].addEventListener("click", function(){
       let span = this.parentElement.parentElement;
-      span.classList.toggle("leftmenu__opencat")
+      let height = 0;
+      let insidecats = span.querySelector(".leftmenu__insidecats");
+      let insidecat = insidecats.querySelectorAll(".leftmenu__insidecat");
+      for(let i = 0; i < insidecat.length; i++){
+        height+= insidecat[i].offsetHeight;
+        console.log(insidecat[i])
+      }
+      insidecats.style.height = height+"px";
+      span.classList.toggle("leftmenu__opencat");
     })
   }
 }
