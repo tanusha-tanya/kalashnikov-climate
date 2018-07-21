2e4c00e939b7a186133cd154a68e711a0bcb0ede
Verevkina T <verevkina@picom.ru>
2018-07-21 17:03:28 +0400

    filters

--- app/templates/modules/categoryfilter/categotyfilter.js
+++ app/templates/modules/categoryfilter/categotyfilter.js
@@ -1,10 +1,42 @@
 const filter = document.querySelector(".categoryfilter");
 if(filter){
+  let buttonblock = filter.querySelector(".categoryfilter__openblock");
+  let all =  buttonblock.querySelector(".categoryfilter__input-all");
+  let buttonFilter =  buttonblock.querySelector(".categoryfilter__input-filter");
+  let labelAll = all.parentNode;
+  let labelFilter = buttonFilter.parentNode;
+  buttonFilter.onchange = function(){
+    if(this.checked){
+      all.checked = false;
+      labelFilter.classList.add("categoryfilter__button-active");
+      labelAll.classList.remove("categoryfilter__button-active");
+      filterOpen()
+    }
+    else{
+      filterClose()
+    }
+  }
+  all.onchange= function(){
+    if(this.checked){
+      buttonFilter.checked = false;
+      labelAll.classList.add("categoryfilter__button-active");
+      labelFilter.classList.remove("categoryfilter__button-active");
+      filterClose()
+      }
+    }
+  }
+function filterOpen(){
+  let hideblock = filter.querySelector(".categoryfilter__hideblock");
+  hideblock.style.display = "block";
   buttonsClick();
   selectOpen();
 }
+function filterClose(){
+  let hideblock = filter.querySelector(".categoryfilter__hideblock");
+  hideblock.style.display = "none";
+}
 function buttonsClick(){
-  let buttonblock = filter.querySelector(".categoryfilter__buttonblock");
+  let buttonblock = filter.querySelector(".categoryfilter__hideblock");
   let input = buttonblock.querySelectorAll("input");
   for(let i=0; i < input.length; i++){
     input[i].addEventListener('change', function(){
@@ -20,7 +52,6 @@ function selectOpen(){
   let select = selectBlock.querySelectorAll(".categoryfilter__select");
   for(let i=0; i < select.length; i++){
     select[i].addEventListener('click', function(){
-      console.log(this)
       let selectitem = this.parentNode;
       let popup = selectitem.querySelector('.categoryfilter__popup');
       if(popup.classList.contains('categoryfilter__popup-open')){
