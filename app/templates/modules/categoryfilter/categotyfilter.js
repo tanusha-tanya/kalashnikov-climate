242565fe129ad54fabc333e2ed8421e546b00f86
Verevkina T <verevkina@picom.ru>
2018-07-24 20:44:35 +0400

    filter

--- app/templates/modules/categoryfilter/categotyfilter.js
+++ app/templates/modules/categoryfilter/categotyfilter.js
@@ -1,29 +1,27 @@
 const filter = document.querySelector(".categoryfilter");
 if(filter){
-  let all =  filter.querySelector(".categoryfilter__input-all");
-  let buttonFilter =  filter.querySelector(".categoryfilter__input-filter");
-  let labelAll = all.parentNode;
-  let labelFilter = buttonFilter.parentNode;
+/*  let buttonFilter =  filter.querySelector(".categoryfilter__input-filter");
+  let catsHide = filter.querySelectorAll(".categoryfilter__button-hide");
+  let catsShow = filter.querySelectorAll(".categoryfilter__button-hide");
+  let selectBlock = filter.querySelector(".categoryfilter__selectblock");
   buttonFilter.onchange = function(){
     if(this.checked){
-      all.checked = false;
-      labelFilter.classList.add("categoryfilter__button-active");
-      labelAll.classList.remove("categoryfilter__button-active");
-      filterOpen()
+      this.parentNode.classList.add("categoryfilter__button-active");
+      selectBlock.classList.remove("categoryfilter__selectblock-hide");
+      selectBlock.classList.add("categoryfilter__selectblock-show");
+      for(let i = 0; i < catsHide.length; i++){
+      catsHide[i].classList.add("categoryfilter__button-show");
+      catsHide[i].classList.remove("categoryfilter__button-hide");
+      }
     }
     else{
-      filterClose()
-    }
-  }
-  all.onchange= function(event){
-    if(this.checked){
-      buttonFilter.checked = false;
-      labelAll.classList.add("categoryfilter__button-active");
-      labelFilter.classList.remove("categoryfilter__button-active");
-      filterClose()
-    }
-    else{
-      event.preventDefault();
+      this.parentNode.classList.remove("categoryfilter__button-active");
+      selectBlock.classList.remove("categoryfilter__selectblock-show");
+      selectBlock.classList.add("categoryfilter__selectblock-hide");
+      for(let i = 0; i < catsShow.length; i++){
+      catsShow[i].classList.add("categoryfilter__button-hide");
+      catsShow[i].classList.remove("categoryfilter__button-show");
+      }
     }
   }
   let dataText = [], actuallyText = [];
@@ -55,48 +53,8 @@ if(filter){
   window.addEventListener("resize", function(){
     changeText();
   })
-}
-
-function filterOpen(){
-  let hideButons = filter.querySelectorAll(".categoryfilter__button-hide");
-  let select = filter.querySelector(".categoryfilter__selectblock-hide");
-  select.classList.remove("categoryfilter__selectblock-hide");
-  select.classList.add("categoryfilter__selectblock-show");
-  for(let i = 0; i < hideButons.length; i++){
-    hideButons[i].classList.remove("categoryfilter__button-hide");
-    hideButons[i].classList.add("categoryfilter__button-show");
-  }
-  buttonsClick();
-  selectOpen();
-}
-
-function filterClose(){
-  let hideButons = filter.querySelectorAll(".categoryfilter__button-show");
-  let select = filter.querySelector(".categoryfilter__selectblock-show");
-  select.classList.remove("categoryfilter__selectblock-show");
-  select.classList.add("categoryfilter__selectblock-hide");
-  for(let i = 0; i < hideButons.length; i++){
-    hideButons[i].classList.remove("categoryfilter__button-show");
-    hideButons[i].classList.add("categoryfilter__button-hide");
-  }
-}
-
-function buttonsClick(){
-  let input = filter.querySelectorAll(".categoryfilter__input");
-  for(let i=0; i < input.length; i++){
-    input[i].addEventListener('change', function(){
-      let label = this.parentNode;
-      this.checked?
-      label.classList.add("categoryfilter__button-active"):
-      label.classList.remove("categoryfilter__button-active")
-    })
-  }
-}
-
-function selectOpen(){
-  let selectBlock = filter.querySelector(".categoryfilter__selectblock");
   let select = selectBlock.querySelectorAll(".categoryfilter__select");
-  let popups =  selectBlock.querySelector('.categoryfilter__popup');
+  let popups =  selectBlock.querySelectorAll('.categoryfilter__popup');
   for(let i=0; i < select.length; i++){
     select[i].addEventListener('click', function(){
       let selectitem = this.parentNode;
@@ -104,40 +62,105 @@ function selectOpen(){
       selectChange(this);
       if(popup.classList.contains('categoryfilter__popup-open')){
         popup.classList.remove('categoryfilter__popup-open');
-        this.classList.remove(".categoryfilter__select-open")
+        this.classList.remove('categoryfilter__select-open');
+        selectChange(this)
       }
       else{
-        for(let i=0; i < popups.length; i++){
-          popups[i].remove('categoryfilter__popup-open');
+        console.log(popups);
+        for(let j=0; j < popups.length; j++){
+          popups[j].classList.remove('categoryfilter__popup-open');
+          select[j].classList.remove('categoryfilter__select-open');
         }
         popup.classList.add('categoryfilter__popup-open');
-        this.classList.add(".categoryfilter__select-open");
+        this.classList.add('categoryfilter__select-open');
       }
     })
   }
-}
+  function selectChange(select){
+    let inputs = select.parentNode.querySelectorAll(".categoryfilter__popupinput");
+    let count = 0;
+    let span = select.querySelector(".categoryfilter__counter");
+    let close = select.querySelector(".categoryfilter__closesvg");
+    for(let i=0; i < inputs.length; i++){
+      inputs[i].addEventListener("change", function(){
+        setCounter();
+      })
+    }
+  }*/
+  let dataText = [], actuallyText = [];
+  let labelSpan = document.querySelectorAll(".categoryfilter__button span");
 
-function selectChange(select){
-  let inputs = select.parentNode.querySelectorAll(".categoryfilter__popupinput");
-  let count = 0;
-  let span = select.querySelector("span");
-  let text = span.textContent;
-  for(let i=0; i < inputs.length; i++){
-    inputs[i].addEventListener("change", function(){
-      if(inputs[i].checked){
-        count++;
+  for(let i = 0; i < labelSpan.length; i++){
+    actuallyText.push(labelSpan[i].textContent)
+    if(labelSpan[i].dataset.text !== undefined){
+      dataText.push(labelSpan[i].dataset.text)
+    }
+    else{
+      dataText.push(labelSpan[i].textContent)
+    }
+  }
+  function changeText(){
+    if(window.innerWidth >= 768){
+      for(let i = 0; i < labelSpan.length; i++){
+        labelSpan[i].textContent = dataText[i];
       }
-      if(!inputs[i].checked){
-        count--;
+    }
+    else{
+      for(let i = 0; i < labelSpan.length; i++){
+        labelSpan[i].textContent = actuallyText[i];
       }
-      if(count > 0){
-        span.textContent = text + ": " + count;
-        select.classList.add("categoryfilter__select-active")
+    }
+  }
+  changeText()
+  window.addEventListener("resize", function(){
+    changeText();
+  })
+  let select = selectBlock.querySelectorAll(".categoryfilter__select");
+  let popups =  selectBlock.querySelectorAll('.categoryfilter__popup');
+  for(let i=0; i < select.length; i++){
+    select[i].addEventListener('click', function(){
+      let selectitem = this.parentNode;
+      let popup = selectitem.querySelector('.categoryfilter__popup');
+      selectChange(this);
+      if(popup.classList.contains('categoryfilter__popup-open')){
+        popup.classList.remove('categoryfilter__popup-open');
+        this.classList.remove('categoryfilter__select-open');
+        selectChange(this)
       }
       else{
-        select.classList.remove("categoryfilter__select-active")
-        span.textContent = text
+        console.log(popups);
+        for(let j=0; j < popups.length; j++){
+          popups[j].classList.remove('categoryfilter__popup-open');
+          select[j].classList.remove('categoryfilter__select-open');
+        }
+        popup.classList.add('categoryfilter__popup-open');
+        this.classList.add('categoryfilter__select-open');
       }
     })
   }
+  function setCounter(){
+    let selects = filter.querySelectorAll(".categoryfilter__select");
+    for(let i = 0; i < selects.length; i++){
+      let count = 0;
+      let inputs = selects[i].querySelectorAll(".categoryfilter__popupinput");
+      let span = selects[i].querySelector(".categoryfilter__counter");
+      for(let i = 0; i < inputs.length; i++){
+        if(inputs[i].checked){
+          count++;
+        }
+        if(!inputs[i].checked){
+          count--;
+        }
+        if(count > 0){
+          span.textContent = ": " + count;
+          selects[i].classList.add("categoryfilter__select-active");
+          //close.style.display = "block";
+        }
+        else{
+          span.textContent = "";
+        }
+      }
+    }
+  }
+  setCounter();
 }
