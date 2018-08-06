91681d8b4eef5fbbdb5b69bc09ca537e5567efe3
Verevkina T <verevkina@picom.ru>
2018-08-06 19:22:09 +0400

    filter again

--- app/templates/modules/categoryfilter/categotyfilter.js
+++ app/templates/modules/categoryfilter/categotyfilter.js
@@ -1,6 +1,12 @@
 const filter = document.querySelector(".categoryfilter");
 if(filter){
   let buttonFilter = filter.querySelector("#filter");
+  let selectBlock = filter.querySelector(".categoryfilter__selectblock");
+  let reset = document.createElement("div");
+  reset.id = "reset";
+  reset.className = "categoryfilter__reset";
+  reset.textContent = "Сбросить";
+
   buttonFilter.onchange = function(){
     let catsHide = filter.querySelectorAll(".categoryfilter__button-hide");
     let selectHide = filter.querySelectorAll(".categoryfilter__selectblock-hide");
@@ -57,8 +63,29 @@ if(filter){
     changeText();
   });
 
-  let select = filter.querySelectorAll(".categoryfilter__select");
+  let select = filter.querySelectorAll('.categoryfilter__select');
   let popups =  filter.querySelectorAll('.categoryfilter__popup');
+
+  document.addEventListener('DOMContentLoaded', function(){
+    for(let i = 0; i < popups.length; i++){
+      let counter = 0;
+      let checkbox = popups[i].querySelectorAll('.categoryfilter__popupinput');
+      for(let j = 0; j < checkbox.length; j++){
+        if(checkbox[j].checked){
+          counter++;
+        }
+      }
+      if(counter > 0){
+        let selectItem = popups[i].closest('.categoryfilter__selectitem');
+        let span = selectItem.querySelector('.categoryfilter__counter');
+        let select = selectItem.querySelector('.categoryfilter__select');
+        let svg = selectItem.querySelector('.categoryfilter__closesvg');
+        span.textContent = ': '+counter;
+        select.classList.add('categoryfilter__select-active');
+        svg.style.display = 'block';
+      }
+    }
+  });
 
   for(let i=0; i < select.length; i++){
     select[i].addEventListener('click', function(){
@@ -84,7 +111,8 @@ if(filter){
     let inputs = select.parentNode.querySelectorAll(".categoryfilter__popupinput");
     let span = select.querySelector(".categoryfilter__counter");
     let close = select.querySelector(".categoryfilter__closesvg");
-    let count = span.textContent !== "" ? span.textContent - 1: 0;
+    let count = span.textContent !== "" ? parseInt(span.textContent.replace(/\D+/g,"")): 0;
+    let popup = select.closest(".categoryfilter__selectitem").querySelector(".categoryfilter__popup ")
     for(let i=0; i < inputs.length; i++){
       inputs[i].addEventListener("change", function(){
         if(this.checked){
@@ -96,19 +124,59 @@ if(filter){
           changeCount(count, select, span)
         }
         if(count>=1){
-          select.classList.add("categoryfilter__select-active")
+          select.classList.add("categoryfilter__select-active");
+          close.style.display="block";
+          selectBlock.appendChild(reset);
         }
         else{
-          select.classList.remove("categoryfilter__select-active")
+          select.classList.remove("categoryfilter__select-active");
+          popup.classList.remove("categoryfilter__popup-open");
+          close.style.display="none"
         }
       })
     }
   }
   function  changeCount(count, select, span){
+    let close = select.querySelector('.categoryfilter__closesvg');
     span.textContent = "";
-    span.textContent = " :"+count;
+    span.textContent = ": "+count;
     if(count <= 0){
       span.textContent = "";
+      select.classList.remove("categoryfilter__select-active");
+      close.style.display = "none";
+    }
+    else{
+      return
     }
   }
+
+  let closeBtn =  filter.querySelectorAll('.categoryfilter__closesvg');
+  for(let i = 0; i < closeBtn.length; i++){
+    closeBtn[i].addEventListener("click", function(){
+      let clearSelect = this.parentNode;
+      clearPopup(clearSelect);
+    })
+  };
+
+  function clearPopup(select) {
+    let popup = select.parentNode.querySelector(".categoryfilter__popup");
+    let inputs = popup.querySelectorAll(".categoryfilter__popupinput");
+    let span = select.querySelector(".categoryfilter__counter")
+    for(let i = 0; i < inputs.length; i++){
+      inputs[i].checked = false;
+      changeCount(0, select, span)
+    }
+  }
+
+  document.body.addEventListener("click", function(event){
+    if(!event.target.closest(".categoryfilter__selectitem")){
+      for(let i = 0; i < select.length; i++){
+        select[i].classList.remove("categoryfilter__select-open");
+      }
+      for(let i = 0; i < popups.length; i++){
+        popups[i].classList.remove("categoryfilter__popup-open");
+      }
+    }
+  })
+
 }
