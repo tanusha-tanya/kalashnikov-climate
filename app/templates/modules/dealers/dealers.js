5f55414b6f7449b196ec07c5da37f5c65edc5bc8
Verevkina T <verevkina@picom.ru>
2018-09-21 14:43:59 +0400

    autocompleet

--- app/templates/modules/dealers/dealers.js
+++ app/templates/modules/dealers/dealers.js
@@ -2,81 +2,107 @@ const dealers = document.querySelector(".dealers__filter");
 if(dealers){
   let field = dealers.querySelectorAll("input[type='text']");
   let fieldHidden = dealers.querySelectorAll("input[type='hidden']");
-  let takeId;
-  let autocompleet = document.createElement("div");
-  autocompleet.className = "autocompleet";
-  let string;
-  let autocompleetString = dealers.querySelectorAll(".autocompleet__string");
-  let name;
-  let hiddenInput;
 
+  //обработка input
   for(let i = 0; i < field.length; i++){
     field[i].addEventListener("input", function(){
-      let val = field[i].value;
-      let keyword = val.toLowerCase().trim();
       let dataList = JSON.parse(this.dataset.list);
-      let forId = this.dataset.for;
-      let names = [];
-      for(let k = 0; k < dataList.length; k++){
-        name = dataList[k].NAME;
-        let currentName = name.toLowerCase().trim();
-        if(currentName.indexOf(keyword) !== -1){
-          if(names.indexOf(name) === -1){
-          names.push(name);
-        }
-        autocompleetShow(names, field[i])
-          /*if(keyword === currentName){
-            takeId = dataList[k].ID;
-            setId(takeId)
-            return
-          }
-          else{
-            takeId = "";
-            setId(takeId)
-          }*/
-        }
-        else if(keyword = ""){
-          if(autocompleet)
-          autocompleetHide(field[i].parentNode)
-        }
-        else{
-          if(autocompleet)
-          autocompleetHide(field[i].parentNode)
-        }
-      }
-
-      function setId(takeId){
-        for(let j = 0; j < fieldHidden.length; j++){
-          if(fieldHidden[j].name === forId){
-            hiddenInput = fieldHidden[j];
-            hiddenInput.value = takeId;
-          }
-        }
-      }
-
+      let names = []
+      let text = field[i].value;
+      let word = text.toLowerCase().trim();
+      matchName(word, this);
     })
   }
 
-  function autocompleetShow(name, field){
-    field.parentNode.appendChild(autocompleet);
-    for(let i = 0; i<name.length; i++){
-      string += "<span class='autocompleet__string'>"+name[i]+"</span>"
+ //устанавливаем значение в скрытом инпуте
+  function SetHiddenId(input, value){
+    let forId = input.dataset.for;
+    for(let i = 0; i < fieldHidden.length; i++){
+      if(fieldHidden[i].name === forId){
+          let hiddenInput = fieldHidden[i];
+          hiddenInput.value = value;
+      }
     }
-      autocompleet.innerHTML = string;
-      stringClick()
-    }
+  }
 
-  function autocompleetHide(that){
-    that.removeChild(autocompleet)
+  //сравниваем введенное значение с именами инпута
+  function matchName(word, input){
+    let takeId;
+    let dataList = JSON.parse(input.dataset.list);
+    let names = [];
+    for(let i = 0; i < dataList.length; i++){
+      let name = dataList[i].NAME.toLowerCase().trim()
+      if(word === name){
+        takeId = dataList[i].ID;
+        SetHiddenId(input, takeId);
+        hideAutocompleet(input);
+        return
+      }
+      else if(word == ""){
+        takeId = "";
+        SetHiddenId(input, takeId);
+        hideAutocompleet(input);
+      }
+      else if(name.indexOf(word) !== -1){
+        takeId = "";
+        SetHiddenId(input, takeId);
+        names.push(dataList[i].NAME);
+      }
+      else{
+        takeId = "";
+        SetHiddenId(input, takeId);
+        hideAutocompleet(input);
+      }
+    }
+    if(names.length > 0){
+      showAutocompleet(names, input);
+    }
+    else{
+      hideAutocompleet(input);
+    }
   }
-  function stringClick(){
-    for(let i =0; i < autocompleetString.length; i++){
-    autocompleetString[i].addEventListener("click", function(){
-      let parent = this.parentNode;
-      let input = parent.parentNode.querySelector("input[type='text']");
-      input.value = this.textContent
-      autocompleetHide(parent.parentNode)
-    })
+
+  //Показываем автокомплит
+  function showAutocompleet(names, input){
+    let autocompleet = input.parentNode.querySelector(".autocompleet");
+    autocompleet.classList.add("autocompleet-active");
+    if(autocompleetString){
+      for(let i = 0; i < autocompleetString.length; i++){
+        autocompleet.removeChild(autocompleetString[i]);
+      }
+    }
+    let string = "";
+    for(let i = 0; i < names.length; i++){
+      string+= '<div class="autocompleet__string">'+names[i]+'</div>'
+    }
+    autocompleet.innerHTML = string;
+    let autocompleetString = autocompleet.querySelectorAll(".autocompleet__string");
+    selectRow(autocompleetString)
   }
-}
+
+  //Убираем автокомплит
+  function hideAutocompleet(input){
+    let autocompleet = input.parentNode.querySelector(".autocompleet");
+    let autocompleetString = autocompleet.querySelectorAll(".autocompleet__string");
+    if(autocompleetString.length > 0){
+      for(let i = 0; i < autocompleetString.length; i++){
+        autocompleet.removeChild(autocompleetString[i]);
+      }
+    }
+    autocompleet.classList.remove("autocompleet-active")
+  }
+
+  //Выбираем строку из списка
+  function selectRow(autocompleetString){
+    for(let i = 0; i< autocompleetString.length; i++){
+      autocompleetString[i].addEventListener("click", function(){
+        let text = this.textContent;
+        let word = text.toLowerCase().trim()
+        let input = this.parentNode.parentNode.querySelector("input[type='text']");
+        matchName(word, input);
+        input.value = text;
+        input.parentNode.classList.add("activeInput");
+      })
+    }
+  }
 }
