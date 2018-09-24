5e79bed58c925231a4fd38daffb081053c285f55
Verevkina T <verevkina@picom.ru>
2018-09-24 19:18:48 +0400

    geolocation

--- app/templates/modules/dealers/dealers.js
+++ app/templates/modules/dealers/dealers.js
@@ -105,4 +105,150 @@ if(dealers){
       })
     }
   }
+
+
+/*  function geoFindMe() {
+   if (!navigator.geolocation){
+      console.log("<p>Geolocation is not supported by your browser</p>")
+     return;
+   }
+
+   function success(position) {
+    if (window.sessionStorage && window.localStorage) {
+      sessionStorage.setItem("latitude", position.coords.latitude)
+      sessionStorage.setItem("longitude", position.coords.longitude)
+    }
+    /*matchCity(latitude, longitude)*/
+   /*};
+
+   function error() {
+     console.log("Unable to retrieve your location");
+   };
+
+   navigator.geolocation.getCurrentPosition(success, error);
+ }*/
+
+  /*function matchCity(latitude, longitude){
+    getCitiesCoords();
+  }*/
+
+  /*function getCitiesCoords(){
+    let datalist;
+    for(let i = 0; i < field.length; i++){
+      datalist = JSON.parse(field[i].dataset.list);
+    }
+      for(let j = 0; j < datalist.length; j++){
+        if(datalist[j].XY !== undefined){
+          for (let m = 0; m < datalist[j].XY.length; m++){
+            getDistance(datalist[j].XY[m].X, datalist[j].XY[m].Y)
+          }
+        }
+    }
+  }
+
+  function getDistance(lat, long){
+    let distance = Math.sqrt(Math.pow((latitude - lat), 2) + Math.pow((longitude - long), 2));
+    distanceArr.push(distance);
+    //let sortDistance = distanceArr.sort(compareNumeric);
+    //let closestPoint = sortDistance[0];
+    //let index = distanceArr.findIndex(el => el.name == 15.577980022704354);
+    let searchName = "15.577980022704354";
+    for (let f = 0; f < distanceArr.length; f++){
+      console.log(distanceArr[f]);
+      /*if(distanceArr[f].indexOf(searchName) !== -1){
+        console.log(f);
+        return
+      }
+    }
+  }
+
+  function compareNumeric(a, b) {
+    if (a > b) return 1;
+    if (a < b) return -1;
+  }*/
+
+  /*getDistance(55.794572, 37.654877)//1200
+  getDistance(59.939095, 30.315868)//1900
+  getDistance(43.117948, 131.948548)//8100
+  getDistance(56.461621, 53.803678)//65
+  getDistance(43.708220, 39.580032)// 2400
+  getDistance(55.796289, 49.108795)//390
+  getDistance(53.195538, 50.101783)//560
+  getDistance(58.010374, 56.229398)//280
+  getDistance(56.838011, 60.597465)//640*/
+
+  //geoFindMe()
+}
+window.onload = function(){
+  if(ymaps){
+    let city = ymaps.geolocation.city;
+    const dealersCity = document.querySelector(".dealers__input-city");
+    if(dealersCity){
+      let cityId;
+      let hiddenInput = document.querySelector('input[name="filter[CITY]"]');
+      let datalist = JSON.parse(dealersCity.dataset.list);
+      let latitude = ymaps.geolocation.latitude;
+      let longitude = ymaps.geolocation.longitude;
+      let distanceArr = [];
+      let oldDistance = [];
+      function isCity(){
+        for(let i =0; i < datalist.length; i++){
+          if(datalist[i].NAME.indexOf(city) !== -1){
+            cityId = datalist[i].ID;
+            return cityId
+          }
+          else{
+              for (let m = 0; m < datalist[i].XY.length; m++){
+              let lat = datalist[i].XY[m].X;
+              let long = datalist[i].XY[m].Y;
+              console.log(lat)
+              console.log(long)
+              let distance = Math.sqrt(Math.pow((latitude - lat), 2) + Math.pow((longitude - long), 2));
+              console.log( distance)
+              distanceArr.push(distance);
+            }
+          }
+        }
+        if(distanceArr.length > 0){
+          sortDistance(distanceArr);
+        }
+        else return
+      }
+
+      isCity()
+
+      function setcityId(cityId){
+        hiddenInput.value = cityId;
+        console.log(hiddenInput)
+      }
+
+
+      function sortDistance(distanceArr){
+        for(let i =0; i < distanceArr.length; i++){
+          oldDistance.push(distanceArr[i])
+
+        }
+        distanceArr.sort(compareNumeric);
+        let index = oldDistance.indexOf(distanceArr[0]);
+        for(let i =0; i < datalist.length; i++){
+          for (let m = 0; m < datalist[i].XY.length; m++){
+            let arrey = [];
+            arrey.push(datalist[i].XY[m]);
+            console.log(arrey)
+            /*if((arrey.length + 1) === index){
+              console.log(arrey)
+              cityId = datalist[i].ID;
+            }*/
+          }
+        }
+      }
+
+      function compareNumeric(a, b) {
+        if (a > b) return 1;
+        if (a < b) return -1;
+      }
+      setcityId(cityId);
+
+   }
+  }
 }
